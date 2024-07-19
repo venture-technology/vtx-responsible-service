413f88fe6a9ba73b42e61c4986a12ac7e10ca070
Gustavo Rodrigues <gustavorodrigueslima2004@gmail.com>
2024-07-19 15:42:04 -0300

    feat: query repository

--- internal/repository/child_repository.go
+++ internal/repository/child_repository.go
@@ -11,7 +11,7 @@ type IChildRepository interface {
 	CreateChild(ctx context.Context, child *models.Child) error
 	GetChild(ctx context.Context, rg *string) (*models.Child, error)
 	FindAllChildren(ctx context.Context, cpf *string) ([]models.Child, error)
-	UpdateChild(ctx context.Context, rg *string) error
+	UpdateChild(ctx context.Context, child *models.Child) error
 	DeleteChild(ctx context.Context, rg *string) error
 }
 
@@ -26,21 +26,79 @@ func NewChildRepository(conn *sql.DB) *ChildRepository {
 }
 
 func (cr *ChildRepository) CreateChild(ctx context.Context, child *models.Child) error {
-	return nil
+	sqlQuery := `INSERT INTO children (name, rg, responsible_id) VALUES ($1, $2, $3)`
+	_, err := cr.db.Exec(sqlQuery, child.Name, child.RG, child.Responsible.ID)
+	return err
 }
 
 func (cr *ChildRepository) GetChild(ctx context.Context, rg *string) (*models.Child, error) {
-	return nil, nil
+	sqlQuery := `SELECT id, name, rg, responsible_id FROM children WHERE rg = $1 LIMIT 1`
+	var child models.Child
+	err := cr.db.QueryRow(sqlQuery, *rg).Scan(&child.ID, &child.Name, &child.RG, &child.Responsible.ID)
+	if err != nil || err == sql.ErrNoRows {
+		return nil, err
+	}
+	return &child, nil
 }
 
 func (cr *ChildRepository) FindAllChildren(ctx context.Context, cpf *string) ([]models.Child, error) {
-	return nil, nil
+	sqlQuery := `SELECT id, name, rg, responsible_id FROM children WHERE responsible_id = $1`
+	rows, err := cr.db.Query(sqlQuery)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var children []models.Child
+	for rows.Next() {
+		var child models.Child
+		err := rows.Scan(
+			&child.ID,
+			&child.Name,
+			&child.RG,
+			&child.Responsible.ID,
+		)
+		if err != nil {
+			return nil, err
+		}
+		children = append(children, child)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return children, nil
 }
 
-func (cr *ChildRepository) UpdateChild(ctx context.Context, rg *string) error {
-	return nil
+func (cr *ChildRepository) UpdateChild(ctx context.Context, child *models.Child) error {
+	sqlQuery := `SELECT name FROM children WHERE rg = $1 LIMIT 1`
+	var currentChild models.Child
+	err := cr.db.QueryRow(sqlQuery, child.RG).Scan(&currentChild.Name)
+	if err != nil || err == sql.ErrNoRows {
+		return err
+	}
+	if child.Name != "" && child.Name != currentChild.Name {
+		currentChild.Name = child.Name
+	}
+	sqlQueryUpdate := `UPDATE children SET name = $1 WHERE rg = $2`
+	_, err = cr.db.ExecContext(ctx, sqlQueryUpdate, currentChild.Name, child.RG)
+	return err
 }
 
 func (cr *ChildRepository) DeleteChild(ctx context.Context, rg *string) error {
-	return nil
+	tx, err := cr.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			_ = tx.Rollback()
+			panic(p)
+		} else if err != nil {
+			_ = tx.Rollback()
+		} else {
+			err = tx.Commit()
+		}
+	}()
+	_, err = tx.Exec("DELETE FROM children WHERE rg = $1", *rg)
+	return err
 }
