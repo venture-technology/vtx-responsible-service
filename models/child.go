1dd1bf57da8e1713e11e6ae9ec51629eaaeda7dd
Gustavo Rodrigues <gustavorodrigueslima2004@gmail.com>
2024-07-25 19:01:41 -0300

    feat: add shift at child object

--- models/child.go
+++ models/child.go
@@ -5,4 +5,5 @@ type Child struct {
 	Name        string      `json:"name"`
 	RG          string      `json:"rg"`
 	Responsible Responsible `json:"responsible"`
+	Shift       string      `json:"shift" validate:"oneof=matutino vespertino noturno"`
 }
